b3ec4fbd57a4a83ba1fa16d3563536d1f6004d9d
Sushi <lucidsushi@users.noreply.github.com>
2017-03-17 18:31:49 -0400

    linear merge attemp1 (linear time unsure)

--- Exercise4_list2.py
+++ Exercise4_list2.py
@@ -31,7 +31,9 @@ def remove_adjacent(nums):
 # pass of both lists.
 def linear_merge(list1, list2):
   # +++your code here+++
-  return
+  #not sure this is "liner" time
+  list1.extend(list2)
+  return sorted(list1)
 
 # Note: the solution above is kind of cute, but unforunately list.pop(0)
 # is not constant time with the standard python list implementation, so
